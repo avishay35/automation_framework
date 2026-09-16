041913057b4aafcecdf3740831f7a0f26370ae10
avishay35 <av1shay.mail35@gmail.com>
2026-09-16 21:00:39 +0300

    Update settings.py fix ignore empty as no .env file in gihub ci

--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,7 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
+        env_ignore_empty=True,  # Ignores empty env strings if they slip through
         extra="ignore"
     )
 
